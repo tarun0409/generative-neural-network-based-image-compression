5b274f1b896aa68793ef266e1003766b3545f271
Ankit <ankit.pant@students.iiit.ac.in>
2019-04-29 15:51:01 +0530

    Seperate models created for different datasets

---
+++ Automobile_model.py
@@ -0,0 +1,221 @@
+import tensorflow as tf
+import numpy as npy
+import pickle
+import datetime
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Flatten, Reshape
+from keras.layers import Conv2D, Conv2DTranspose, UpSampling2D,MaxPooling2D
+from keras.layers import LeakyReLU, Dropout
+from keras.layers import BatchNormalization
+from keras.optimizers import Adam, RMSprop, SGD
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from PIL import Image
+
+class DCGAN(object):
+
+    def __init__(self,img_rows=32, img_cols=32, img_chanl=3):
+        self.image_rows = img_rows
+        self.image_columns = img_cols
+        self.image_channels = img_chanl
+        self.Discriminator = None
+        self.Generator = None
+        self.Discriminator_Model = None
+        self.Adversarial_Model = None
+        self.Compressor_Model  = None
+
+    def generator(self):
+        if self.Generator:
+            return self.Generator
+        self.Generator = Sequential()
+        dropout = 0.4
+        dimen = 8
+        depth = 96
+        self.Generator.add(Dense(dimen*dimen*depth,input_dim=3072))
+        self.Generator.add(LeakyReLU(alpha=0.05))
+        # self.Generator.add(Activation('relu'))
+        self.Generator.add(Reshape((dimen, dimen, depth)))
+        self.Generator.add(Dropout(dropout))
+
+        self.Generator.add(UpSampling2D())
+        self.Generator.add(Conv2DTranspose(int(depth/2), 5, padding='same'))
+        self.Generator.add(LeakyReLU(alpha=0.05))
+        # self.Generator.add(Activation('relu'))
+
+        self.Generator.add(UpSampling2D())
+        self.Generator.add(Conv2DTranspose(int(depth/4), 5, padding='same'))
+        self.Generator.add(LeakyReLU(alpha=0.05))
+        # self.Generator.add(Activation('relu'))
+
+        self.Generator.add(Conv2DTranspose(int(depth/8), 5, padding='same'))
+        self.Generator.add(LeakyReLU(alpha=0.05))
+        
+        self.Generator.add(Conv2DTranspose(int(depth/16), 5, padding='same'))
+        self.Generator.add(LeakyReLU(alpha=0.05))
+        # self.Generator.add(Activation('relu'))
+
+        self.Generator.add(Conv2DTranspose(int(depth/32), 5, padding='same'))
+#         self.Generator.add(LeakyReLU(alpha=0.05))
+        
+        self.Generator.add(Activation('sigmoid'))
+        print("Generator Summary")
+        self.Generator.summary()
+        return self.Generator
+
+
+    def discriminator(self):
+        if self.Discriminator:
+            return self.Discriminator
+        self.Discriminator = Sequential()
+        depth = 64
+        dropout = 0.4
+        input_shape = (self.image_rows, self.image_columns, self.image_channels)
+
+        self.Discriminator.add(Conv2D(6,5,strides=(2,2),padding='same',input_shape=input_shape))
+        self.Discriminator.add(LeakyReLU(alpha=0.05))
+        self.Discriminator.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        self.Discriminator.add(Dropout(0.25))
+        self.Discriminator.add(Conv2D(6,5,strides=(2,2),padding='same'))
+        self.Discriminator.add(LeakyReLU(alpha=0.05))
+        self.Discriminator.add(MaxPooling2D(pool_size=(2,2),strides=(2,2)))
+        self.Discriminator.add(Flatten())
+        self.Discriminator.add(Dense(120))
+        self.Discriminator.add(LeakyReLU(alpha=0.05))
+        self.Discriminator.add(Dense(84))
+        self.Discriminator.add(LeakyReLU(alpha=0.05))
+        self.Discriminator.add(Dense(1))
+        self.Discriminator.add(Activation('sigmoid'))
+        print("Discriminator Summary")
+        self.Discriminator.summary()
+        return self.Discriminator
+#         self.Discriminator.add(Flatten())
+#         self.Discriminator.add(Dense(512))
+#         self.Discriminator.add(LeakyReLU(alpha=0.05))
+#         self.Discriminator.add(Dropout(0.5))
+        # self.Discriminator.add(Dense(NB_CLASSES))
+        # self.Discriminator.add(Activation('softmax'))
+
+
+
+        # self.Discriminator.add(Conv2D(depth*1, 5, strides=2, input_shape=input_shape, padding='same'))
+        # self.Discriminator.add(LeakyReLU(alpha=0.2))
+        # self.Discriminator.add(Dropout(dropout))
+
+        # self.Discriminator.add(Conv2D(depth*2, 5, strides=2, padding='same'))
+        # self.Discriminator.add(LeakyReLU(alpha=0.2))
+        # self.Discriminator.add(Dropout(dropout))
+
+        # self.Discriminator.add(Conv2D(depth*4, 5, strides=2, padding='same'))
+        # self.Discriminator.add(LeakyReLU(alpha=0.2))
+        # self.Discriminator.add(Dropout(dropout))
+
+        # self.Discriminator.add(Conv2D(depth*8, 5, strides=1, padding='same'))
+        # self.Discriminator.add(LeakyReLU(alpha=0.2))
+        # self.Discriminator.add(Dropout(dropout))
+
+        # Output: 1-dim probability
+        
+        # self.Discriminator.add(Dense(28))
+        
+
+    def discriminator_model(self):
+        if self.Discriminator_Model:
+            return self.Discriminator_Model
+        optimizer = RMSprop(lr=0.0002, decay=6e-8)  
+        self.Discriminator_Model = Sequential()
+        self.Discriminator_Model.add(self.discriminator())
+        self.Discriminator_Model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return self.Discriminator_Model
+
+    def adversarial_model(self):
+        if self.Adversarial_Model:
+            return self.Adversarial_Model
+        optimizer = RMSprop(lr=0.0001, decay=3e-8)  
+        self.Adversarial_Model = Sequential()
+        self.Adversarial_Model.add(self.generator())
+        self.Adversarial_Model.add(self.discriminator())
+        self.Adversarial_Model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+        return self.Adversarial_Model
+
+
+class CIFAR(object):
+
+    def __init__(self):
+        self.img_rows = 32
+        self.img_cols = 32
+        self.img_chanl = 3
+
+#         self.x_train = input_data.read_data_sets("mnist", one_hot=True).train.images
+#         self.x_train = self.x_train.reshape(-1, self.img_rows, self.img_cols, 1).astype(npy.float32)
+        airfile = open('./automobile.pickle', 'rb') 
+        self.x_train = pickle.load(airfile)
+        airfile.close()
+        self.x_train = self.x_train.astype(npy.float32)
+
+        self.DCGAN = DCGAN()
+        self.generator = self.DCGAN.generator()
+        self.discriminator = self.DCGAN.discriminator_model()
+        self.adversary = self.DCGAN.adversarial_model()
+
+    def train(self, train_steps=5000, batch_size=256):
+
+        # noise_input = npy.random.uniform(-1,1,size=[16, 784])
+        for i in range(train_steps):
+            images_train = self.x_train[npy.random.randint(0,self.x_train.shape[0], size=batch_size), :, :, :]
+#             print("Images train",npy.shape(images_train))
+            if(i==0):
+              print(npy.shape(images_train))
+              img = images_train[0]/255
+#               image = npy.reshape(images_train[0], [self.img_rows, self.img_cols,self.img_chanl])
+#               print(images_train[0])
+              plt.imshow(img)
+              plt.show()
+            noise = npy.random.uniform(-1.0, 1.0, size=[batch_size, 3072]) #changed
+            images_gen = self.generator.predict(noise)
+#             print("Images Gen",npy.shape(images_gen))
+            x = npy.concatenate((images_train, images_gen))
+            y = npy.ones([2*batch_size, 1])
+            y[batch_size:, :] = 0
+            d_loss = self.discriminator.train_on_batch(x,y)
+
+            y = npy.ones([batch_size, 1])
+            noise = npy.random.uniform(-1.0, 1.0, size=[batch_size, 3072]) #changed
+            a_loss = self.adversary.train_on_batch(noise, y)
+
+            log_mesg = "%d: [D loss: %f, acc: %f]" % (i, d_loss[0], d_loss[1])
+            log_mesg = "%s  [A loss: %f, acc: %f]" % (log_mesg, a_loss[0], a_loss[1])
+            print(log_mesg)
+
+            if (i%100==0):
+                noisy = npy.random.uniform(-1.0, 1.0, size=[1, 3072]) #changed
+                images = self.generator.predict(noisy)
+                image = npy.reshape(images, [self.img_rows, self.img_cols,self.img_chanl])
+                plt.imshow(image)
+                plt.axis('off')
+                plt.imsave('/content/drive/My Drive/smai_cifar/autos_gen_cifar.png',image)
+                plt.tight_layout()
+                plt.show()
+                self.generator.save_weights('/content/drive/My Drive/smai_cifar/autos_generator_weights.h5')
+                self.discriminator.save_weights('/content/drive/My Drive/smai_cifar/autos_discriminator_weights.h5')
+                print('Weights last saved at :')
+                print(datetime.datetime.now())
+                
+            
+        
+        
+        #Saving weights of model
+        self.generator.save_weights('/content/drive/My Drive/smai_cifar/autos_generator_weights.h5')
+        self.discriminator.save_weights('/content/drive/My Drive/smai_cifar/autos_discriminator_weights.h5')
+
+
+if __name__ == "__main__":
+    print('Program started at:')
+    print(datetime.datetime.now())
+    cifar_dcgan = CIFAR()
+    cifar_dcgan.train(train_steps=30001, batch_size=256)
+    print('Program ended at :')
+    print(datetime.datetime.now())
--- compress.py
+++ compress.py
@@ -1,7 +1,7 @@
 import sys
 import numpy as npy
 import tensorflow as tf
-
+import pickle
 from tensorflow.examples.tutorials.mnist import input_data
 
 from keras.models import Sequential
@@ -14,28 +14,47 @@ from keras.optimizers import Adam, RMSprop, SGD
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
-import MNIST_model as MM
+import Airplane_model as AM
+import Flower_model as FM
+import Automobile_model as AT
 
 
-def Compress_MNIST():
-    mnist_gan = MM.MNIST()
-    mnist_gan.generator.load_weights('./generator_weights.h5')
-    mnist_gan.discriminator.load_weights('./discriminator_weights.h5')
-    noise_input = npy.random.uniform(-1.0, 1.0, size=[1, 784])
-    gen_image = mnist_gan.generator.predict(noise_input)
-    orig_image = Image.open('./orig_mnist.png').convert('L')
+def Compress_Airplane():
+    cifar_gan = AM.CIFAR()
+    cifar_gan.generator.load_weights('./airplane_generator_weights.h5')
+    cifar_gan.discriminator.load_weights('./airplane_discriminator_weights.h5')
+    noise_input = npy.random.uniform(-1.0, 1.0, size=[1, 3072])
+    gen_image = cifar_gan.generator.predict(noise_input)
+    airfile = open('./airplane_test.pickle', 'rb') 
+    x_train = pickle.load(airfile)
+    airfile.close()
+    r_index = npy.random.randint(0,len(x_train))
+    orig_image = x_train[r_index]
     
     Compress_M = Sequential()
-    Compress_M.add(Conv2D(8, 5, strides=1, input_shape=(28,28,1),padding='same'))
-    Compress_M.add(MaxPooling2D(3,1,padding='same'))
-    Compress_M.add(Conv2D(8, 5, strides=2,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=1, input_shape=(32,32,3),padding='same'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
 #     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
+    Compress_M.add(Activation('sigmoid'))
+    #     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.5))
+
     Compress_M.add(Flatten())
-    # Compress_M.add(Dense(500,input_shape=784))
+    Compress_M.add(Dense(2048))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.5))
+    Compress_M.add(Dense(3072))
+#     Compress_M.add(Dense(500,input_shape=784))
     # Compress_M.add(LeakyReLU(alpha=0.05))
-    Compress_M.add(Dense(784))
+#     Compress_M.add(UpSampling2D())
+#     Compress_M.add(Conv2DTranspose(1, 15, padding='same'))
 #     Compress_M.add(Linear())
-    Compress_M.compile(loss='cosine_proximity', optimizer='adam', metrics=['accuracy'])
+    Compress_M.compile(loss='cosine_proximity', optimizer='Adadelta', metrics=['accuracy'])
 
     x = npy.array(gen_image)
     y = npy.array(orig_image)
@@ -44,22 +63,147 @@ def Compress_MNIST():
     print(npy.shape(y))
     
 #     x = npy.reshape(x,(28,28,1))
-    y = npy.reshape(y,(1,784))
+    y = npy.reshape(y,(1,3072))
     
     
-    Compress_M.fit(x,y,epochs=100)
+    Compress_M.fit(x,y,epochs=150,verbose=1)
     out_img = Compress_M.predict(x)
     print(npy.shape(out_img))
     
-    comp_img = npy.reshape(out_img,(28,28))
-    gen_img = npy.reshape(gen_image,(28,28))
-    plt.imshow(gen_img,cmap='gray')
+    comp_img = npy.reshape(out_img,(32,32,3))
+    gen_img = npy.reshape(gen_image,(32,32,3))
+    plt.imshow(gen_img)
     plt.show()
-    plt.imshow(orig_image,cmap='gray')
+    plt.imshow(orig_image)
     plt.show()
-    plt.imshow(comp_img,cmap='gray')
-    plt.imsave('./comp_img.png',comp_img,cmap='gray')
+    plt.imsave('./orig_image1.png',orig_image)
+    plt.imshow(comp_img)
+    plt.imsave('./airplane_comp_img1.png',comp_img)
     plt.show()
+
+def Compress_Automobile():
+    cifar_gan = AT.CIFAR()
+    cifar_gan.generator.load_weights('./autos_generator_weights.h5')
+    cifar_gan.discriminator.load_weights('./autos_discriminator_weights.h5')
+    noise_input = npy.random.uniform(-1.0, 1.0, size=[1, 3072])
+    gen_image = cifar_gan.generator.predict(noise_input)
+    airfile = open('./automobile_test.pickle', 'rb') 
+    x_train = pickle.load(airfile)
+    airfile.close()
+    r_index = npy.random.randint(0,len(x_train))
+    orig_image = x_train[r_index]
+    Compress_M = Sequential()
+    Compress_M.add(Conv2D(1, 8, strides=1, input_shape=(32,32,3),padding='same'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
+#     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
+    Compress_M.add(Activation('sigmoid'))
+    #     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.5))
+
+    Compress_M.add(Flatten())
+    Compress_M.add(Dense(2048))
+#     Compress_M.add(Activation('relu'))
+    Compress_M.add(LeakyReLU(alpha=0.5))
+    Compress_M.add(Dense(3072))
+#     Compress_M.add(Dense(500,input_shape=784))
+    # Compress_M.add(LeakyReLU(alpha=0.05))
+#     Compress_M.add(UpSampling2D())
+#     Compress_M.add(Conv2DTranspose(1, 15, padding='same'))
+#     Compress_M.add(Linear())
+    Compress_M.compile(loss='cosine_proximity', optimizer='Nadam', metrics=['accuracy'])
+
+    x = npy.array(gen_image)
+    y = npy.array(orig_image)
+    
+    print(npy.shape(x))
+    print(npy.shape(y))
+    
+#     x = npy.reshape(x,(28,28,1))
+    y = npy.reshape(y,(1,3072))
+    
+    
+    Compress_M.fit(x,y,epochs=50,verbose=1)
+    out_img = Compress_M.predict(x)
+    print(npy.shape(out_img))
+    
+    comp_img = npy.reshape(out_img,(32,32,3))
+    gen_img = npy.reshape(gen_image,(32,32,3))
+    plt.imshow(gen_img)
+    plt.show()
+    plt.imshow(orig_image)
+    plt.show()
+    plt.imsave('./orig_image1.png',orig_image)
+    plt.imshow(comp_img)
+    plt.imsave('./auto_comp_img1.png',comp_img)
+    plt.show()
+
+def Compress_Flowers():
+    flow_gan = FM.Flower_model()
+    flow_gan.generator.load_weights('./flowers_generator_weights.h5')
+    flow_gan.discriminator.load_weights('./flowers_discriminator_weights.h5')
+    noise_input = npy.random.uniform(-1.0, 1.0, size=[1, 3072])
+    gen_image = flow_gan.generator.predict(noise_input)
+    airfile = open('./flowers_resized_test.pickle', 'rb') 
+    x_train = pickle.load(airfile)
+    airfile.close()
+    r_index = npy.random.randint(0,len(x_train))
+    orig_image = x_train[r_index]
+    
+    Compress_M = Sequential()
+    Compress_M.add(Conv2D(1, 8, strides=1, input_shape=(32,32,3),padding='same'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
+#     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.05))
+    Compress_M.add(Activation('sigmoid'))
+    #     Compress_M.add(MaxPooling2D(3,1,padding='same'))
+    Compress_M.add(Conv2D(1, 8, strides=2,padding='same'))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.5))
+
+    Compress_M.add(Flatten())
+    Compress_M.add(Dense(2048))
+    Compress_M.add(Activation('sigmoid'))
+#     Compress_M.add(LeakyReLU(alpha=0.5))
+    Compress_M.add(Dense(3072))
+#     Compress_M.add(Dense(500,input_shape=784))
+    # Compress_M.add(LeakyReLU(alpha=0.05))
+#     Compress_M.add(UpSampling2D())
+#     Compress_M.add(Conv2DTranspose(1, 15, padding='same'))
+#     Compress_M.add(Linear())
+    Compress_M.compile(loss='cosine_proximity', optimizer='Adadelta', metrics=['accuracy'])
+
+    x = npy.array(gen_image)
+    y = npy.array(orig_image)
+    
+    print(npy.shape(x))
+    print(npy.shape(y))
+    
+#     x = npy.reshape(x,(28,28,1))
+    y = npy.reshape(y,(1,3072))
+    
+    
+    Compress_M.fit(x,y,epochs=150,verbose=1)
+    out_img = Compress_M.predict(x)
+    print(npy.shape(out_img))
+    
+    comp_img = npy.reshape(out_img,(32,32,3))
+    gen_img = npy.reshape(gen_image,(32,32,3))
+    plt.imshow(gen_img)
+    plt.show()
+    plt.imshow(orig_image)
+    plt.show()
+    plt.imsave('./orig_image1.png',orig_image)
+    plt.imshow(comp_img)
+    plt.imsave('./flow_comp_img1.png',comp_img)
+    plt.show()
+    
 
 if __name__ == "__main__":
     if(len(sys.argv) < 3):
@@ -67,16 +211,11 @@ if __name__ == "__main__":
         sys.exit()
     image_path = sys.argv[1]
     dataset = sys.argv[2]
-    if dataset == 'mnist':
-        Compress_MNIST()
-    else:    
-        generator_weights_path = dataset + '_generator_weights.h5'
-        discriminator_weights = dataset + '_discriminator_weights.h5'
-        model_obj = None
-        orig_image = Image.open(image_path)
-        dims = np.shape(np.asarray(orig_image))
-        tot_dim = 1
-        for d in dims:
-            tot_dim *= d
+    if dataset == 'automobiles':
+        Compress_Automobile()
+    elif dataset == 'airplanes':
+        Compress_Airplane()
+    elif dataset == 'flowers':
+        Compress_Flowers()
         
 
